014c153231746d28420f73556e2c54c72105ba54
Rajput5847 <divyanshchauhanrajput5847@gmail.com>
2023-11-07 16:12:48 +0530

    Shery Bhai

--- script.js
+++ script.js
@@ -69,3 +69,22 @@ Shery.imageEffect("#bimg", {
   config: {"a":{"value":2,"range":[0,30]},"b":{"value":-0.98,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":1.9261871380100277},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.13,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2],"_gsap":{"id":49}},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
   gooey: true
 })
+
+document.querySelector("#ftext button")
+.addEventListener("mouseover",  function () {
+  gsap.to("#future video", {
+    opacity: 1,
+    duration: 1,
+    ease: Power4
+  })
+})
+
+document.querySelector("#ftext button")
+.addEventListener("mouseleave",  function () {
+  gsap.to("#future video", {
+    opacity: 0,
+    duration: 1,
+    ease: Power4
+  })
+  
+})
